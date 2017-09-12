0ace4bcd44fbf7d31c4eb5ebeac584cf2ea3a7db
Odunayo Obajuluwa <odunayoobajuluwa@Odunayos-MacBook-Pro.local>
2017-09-12 17:00:05 -0500

    modified app.js file

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '3001';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {Http, Response } from '@angular/http';
+import { Router } from '@angular/router';
+
+class User {
+	id: number;
+	firstname: string;
+	lastname: string;
+	email: string;
+	username: string;
+	password: string;
+	address: string;
+	city: string;
+	state: string;
+	zip: number;
+}
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+	newUser: User = new User();
+
+  	constructor(private http: Http, private router: Router) {
+  		
+  	}
+
+  	register(){
+	    this.http.post('http://localhost:9393/users/register', this.newUser).subscribe(response => {
+	      window.localStorage.setItem("token", response.json().token)
+	      window.localStorage.setItem("user_id", response.json().id)
+	      this.router.navigate(['/address/', response.json().id])
+	    })
+	  }
+}
